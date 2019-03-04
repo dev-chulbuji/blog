15c90c65b019bff82fb86981b6bb1826af2412aa
dev-chulbuji <dev.chulbuji@gmail.com>
2019-03-04 16:21:00 +0900

    feat: change gatsby-config siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `DEV blog`,
+    author: `DJ Kim`,
+    description: `dev blog`,
+    siteUrl: `https://chulbuji.gq/`,
     social: {
-      twitter: `kylemathews`,
+      github: `dev-chulbuji`
     },
   },
   plugins: [
